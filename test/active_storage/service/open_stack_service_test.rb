93c03060c3ec14f214ef7bd5d9e9172fa5b87e9c
Daniel Jagszent <daniel@jagszent.de>
2018-07-30 23:08:51 +0200

    Refactor Content Type, Filename and Content Disposition handling
    * Fix headers_for_direct_upload: The filename parameter was missing for headers_for_direct_upload. This prevented direct upload from working
    * Fix url: The url method used Fog::Storage::OpenStack.get_object_https_url wrongly
    * Fix headers_for_direct_upload: The Content-Length header is redundant and e.g. Chrome does not allow to set this header anyways.
    * Change upload: set the initial content type of the object as a best guess.
    * Add change_content_type: Add method to change the content type of an existing object.
    * Add Rails initializer that hooks the change_content_type method up
    * Add the default entry point (activestorage-openstack.rb) so that our Railtie gets loaded
    * Add some tests

--- test/active_storage/service/open_stack_service_test.rb
+++ test/active_storage/service/open_stack_service_test.rb
@@ -52,6 +52,13 @@ if SERVICE_CONFIGURATIONS[:openstack]
       assert_equal FIXTURE_DATA, @service.download(FIXTURE_KEY)
     end
 
+    test "correct metadata" do
+      url = @service.url(FIXTURE_KEY, expires_in: 5.minutes,
+                           disposition: :inline,
+                           filename: ActiveStorage::Filename.new("avatar.png"), content_type: "image/png")
+      asset_metadata url, content_type: "image/png", content_length: FIXTURE_DATA.bytesize, filename: "avatar.png", disposition: "inline"
+    end
+
     test "downloading in chunks" do
       key = SecureRandom.base58(24)
       chunk_size = @service.client.instance_values["connection_options"][:chunk_size] || 1.megabyte # default for OpenStack
@@ -82,6 +89,8 @@ if SERVICE_CONFIGURATIONS[:openstack]
                                       filename: ActiveStorage::Filename.new("avatar.png"), content_type: "image/png")
 
       assert_match SERVICE_CONFIGURATIONS[:openstack][:container], url
+      assert_match "avatar.png", url
+      assert_match "inline", url
     end
 
     test "direct upload" do
@@ -89,25 +98,35 @@ if SERVICE_CONFIGURATIONS[:openstack]
         key      = SecureRandom.base58(24)
         data     = "Something else entirely!"
         checksum = Digest::MD5.base64digest(data)
-        hex_checksum = Digest::MD5.hexdigest(data) # For OpenStack
 
         url = @service.url_for_direct_upload(key,
                                              expires_in: 5.minutes,
                                              content_type: "text/plain",
-                                             content_length: data.size,
+                                             content_length: data.bytesize,
                                              checksum: checksum)
+        headers = @service.headers_for_direct_upload(key, 
+                                                     filename: ActiveStorage::Filename.new("something.txt"),
+                                                     content_type: "text/plain",
+                                                     content_length: data.bytesize,
+                                                     checksum: checksum)
 
         uri = URI.parse url
         request = Net::HTTP::Put.new uri.request_uri
         request.body = data
-        request.add_field "Content-Type", "text/plain"
-        request.add_field "Etag", hex_checksum
-        request.add_field "Content-Length", data.size
+        request.add_field "Content-Length", data.bytesize
+        headers.each_pair do |key, val|
+          request.add_field key, val
+        end
         Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
           http.request request
         end
 
         assert_equal data, @service.download(key)
+
+        url = @service.url(key, expires_in: 5.minutes,
+                           disposition: :attachment,
+                           filename: ActiveStorage::Filename.new("something.txt"), content_type: "text/plain")
+        asset_metadata url, content_type: "text/plain", content_length: data.bytesize, filename: "something.txt", disposition: "attachment"
       ensure
         @service.delete key
       end
@@ -143,6 +162,36 @@ if SERVICE_CONFIGURATIONS[:openstack]
         @service.delete("a/a/a")
         @service.delete("a/a/b")
         @service.delete("a/b/a")
+      end
+    end
+
+    test "change_content_type" do
+      key = SecureRandom.base58(24)
+      begin
+        @service.upload(key, StringIO.new(FIXTURE_DATA))
+        @service.change_content_type(key, "text/plain")
+        url = @service.url(key, expires_in: 5.minutes,
+                           disposition: :attachment,
+                           filename: ActiveStorage::Filename.new("something.txt"))
+        asset_metadata url, content_type: "text/plain"
+        @service.change_content_type(key, "application/octet-stream")
+        url = @service.url(key, expires_in: 5.minutes,
+                           disposition: :attachment,
+                           filename: ActiveStorage::Filename.new("something.txt"))
+        asset_metadata url, content_type: "application/octet-stream"
+      ensure
+        @service.delete(key)
+      end
+    end
+
+    def asset_metadata url, content_type: nil, content_length: nil, filename: nil, disposition: nil
+      uri = URI.parse url
+      Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+        response = http.head uri.request_uri
+        assert_equal content_type.to_s, response['content-type'] unless content_type.nil?
+        assert_equal content_length.to_s, response['content-length'] unless content_length.nil?
+        assert_match filename.to_s, response['content-disposition'] unless filename.nil?
+        assert_match disposition.to_s, response['content-disposition'] unless disposition.nil?
       end
     end
   end
